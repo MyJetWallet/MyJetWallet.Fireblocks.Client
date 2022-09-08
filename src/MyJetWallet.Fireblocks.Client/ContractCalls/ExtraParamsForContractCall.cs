7adc0421dd018f3e33618a5d838691de33504737
Aleksandr Belousov <aleksandr.b@simple.app>
2022-09-08 18:13:00 +0300

    contract calls

---
+++ src/MyJetWallet.Fireblocks.Client/ContractCalls/ExtraParamsForContractCall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJetWallet.Fireblocks.Client.ContractCalls
+{
+    public class ExtraParamsForContractCall
+    {
+        [Newtonsoft.Json.JsonProperty("contractCallData",
+            Required = Newtonsoft.Json.Required.DisallowNull,
+            NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string ContractCallData { get; set; }
+    }
+}
